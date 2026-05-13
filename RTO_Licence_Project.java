a07db9b36ecd97ebe695d519bd14efe9fcbe9690
dagadeakshay77-alt <dagadeakshay77@gmail.com>
2026-05-13 18:35:55 +0530

    Add files via upload

---
+++ RTO_Licence_Project.java
@@ -0,0 +1,32 @@
+package rto_project;
+
+import java.util.Scanner;
+
+public class Test {
+	
+	public static void main (String[]args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter Your age:"); {
+			
+		int age = sc.nextInt(); 
+			
+			if(age>=18 && age<=75) {
+				
+				System.out.println("Your Elligible for licence");
+			} else if (age<18) {
+				
+				int wait=18-age;
+				
+				System.out.println("You are not Elligible for licence");
+				System.out.println("Wait for"+wait +"Years of licence");
+			} else {
+				
+				System.out.println("Age bar");
+				System.out.println("Not Elligible for Licence");
+			}
+			sc.close();
+		}
+	}
+}
