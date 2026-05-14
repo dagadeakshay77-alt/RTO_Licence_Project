72fc593584be4b5dce416d7fa036ef32ab7a29d8
dagadeakshay77-alt <dagadeakshay77@gmail.com>
2026-05-14 09:36:54 +0530

    Update RTO_Licence_Project.java

--- RTO_Licence_Project.java
+++ RTO_Licence_Project.java
@@ -2,31 +2,31 @@ package rto_project;
 
 import java.util.Scanner;
 
-public class RTO_Licence_Project {
+public class Test {
 	
 	public static void main (String[]args) {
 		
-		Scanner sc = new Scanner(System.in);
-		
-		System.out.println("Enter Your age:"); {
+		Scanner sc = new Scanner(System.in); 
 			
-		int age = sc.nextInt(); 
+			System.out.println("Enter Your age:");
 			
-			if(age>=18 && age<=75) {
-				
-				System.out.println("Your Elligible for licence");
-			} else if (age<18) {
-				
-				int wait=18-age;
-				
-				System.out.println("You are not Elligible for licence");
-				System.out.println("Wait for"+wait +"Years of licence");
-			} else {
-				
-				System.out.println("Age bar");
-				System.out.println("Not Elligible for Licence");
-			}
-			sc.close();
-		}
+			int age =sc.nextInt();
+		    if (age>=18 && age<=75) {
+		    	
+		    	System.out.println("You are eligible for licence");
+		    }
+		    else if (age<18) {
+		    	
+		    	int wait = 18-age;
+		    	
+		    		System.out.println("You are not eligible for licence");
+		    		System.out.println("Wait for "+wait +"Years for the licence");
+		    	}
+		    	else
+		    	{
+		    		System.out.println("Age bar");
+		    		System.out.println("You are not eligible for licence");
+		    	}
+		    	sc.close();
 	}
 }
